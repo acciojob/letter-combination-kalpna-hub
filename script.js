4d9066fd45ba3f79880b0028eca13d2e7750f444
kalpna-hub <122824448+kalpna-hub@users.noreply.github.com>
2023-03-22 11:47:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,37 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-	if(input_digit.length == 0) {
-		var arr = [];
-		arr.push("");
-		return arr;
-	}
-	let fd = input_digit[0];
-	let ans = [];
-	let ros = input_digit.substring(1);
-	let str = obj11[fd];
-	for(let i=0; i<str.length; i++){
-		let fc = str[i];
-		let smallans = letterCombinations(ros);
-		for (var val of smallans){
-			let smans = fc + val;
-			ans.push(smans);
-		}
-	}
-	return ans;
+function letterCombinations(digits) {
+  const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+
+  if (digits.length === 0) {
+    return [];
+  }
+
+  const result = [];
+
+  function backtrack(currentString, nextDigits) {
+    if (nextDigits.length === 0) {
+      result.push(currentString);
+      return;
+    }
+
+    const letters = digitToLetters[nextDigits[0]];
+
+    for (let i = 0; i < letters.length; i++) {
+      const letter = letters[i];
+      backtrack(currentString + letter, nextDigits.slice(1));
+    }
+  }
+
+  backtrack('', digits);
+  return result.sort();
 }
+
 module.exports = letterCombinations;
